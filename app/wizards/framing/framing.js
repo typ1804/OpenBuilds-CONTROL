5f0e2894794951f72266b6cc545063602c78b1c4
stefan.kramer <stefan.kramer@connect-io.de>
2025-03-25 21:49:46 +0100

    added new framing wizard. create gcode for framing stock

--- app/wizards/framing/framing.js
+++ app/wizards/framing/framing.js
@@ -38,6 +38,23 @@ var framingWizardTemplate = `
             </div>
           </div>
 
+          <div class="row mb-2 pb-2 border-bottom bd-gray">
+           <label class="cell-sm-6">Framing Sides</label>
+           <div class="cell-sm-6">
+             <select id="framingSides" data-role="input" data-clear-button="false">
+               <option value="all" selected>Along all sides</option>
+               
+               <option value="left">Along left side only</option>
+               <option value="top">Along top side only</option>
+               <option value="right">Along right side only</option>
+               <option value="bottom">Along bottom side only</option>
+               
+                <option value="leftRight">Along left and right side only</option>
+                <option value="topBottom">Along top and bottom side only</option>
+             </select>
+           </div>
+         </div>
+
           <div class="row mb-2">
             <label class="cell-sm-6">Cut Depth per Pass</label>
             <div class="cell-sm-6">
@@ -108,9 +125,11 @@ function populateFramingToolForm() {
     });
 
     if (localStorage.getItem("lastFramingTool")) {
-        var data = JSON.parse(localStorage.getItem("lastFramingTool"));
+        var dataRaw = localStorage.getItem("lastFramingTool");
+        var data = JSON.parse(dataRaw);
 
         console.log('storage config')
+        console.dir(dataRaw)
         console.dir(data)
     } else {
         var data = {
@@ -121,7 +140,8 @@ function populateFramingToolForm() {
             framingDepth: 3,
             framingFinalDepth: 3,
             framingCoolant: "enabled",
-            framingRPM: 1000
+            framingRPM: 1000,
+            framingSides: "all"
         };
 
         console.log('default config')
@@ -133,7 +153,6 @@ function populateFramingToolForm() {
     $("#framingY").val(data.framingY);
     $("#framingDepth").val(data.framingDepth);
     if (data.framingFinalDepth !== undefined) {
-        data.framingFinalDepth = data.framingDepth;
         $("#framingFinalDepth").val(data.framingFinalDepth);
     } else {
         $("#framingFinalDepth").val(data.framingDepth);
@@ -142,6 +161,9 @@ function populateFramingToolForm() {
         $('#framingCoolant').val(data.framingCoolant)
     }
     $('#framingRPM').val(data.framingRPM)
+    if (data.framingSides !== undefined) {
+        $('#framingSides').val(data.framingSides);
+    }
 }
 
 function createFramingGcode() {
@@ -154,8 +176,8 @@ function createFramingGcode() {
         framingFinalDepth: parseFloat($("#framingFinalDepth").val()),
         framingRPM: parseFloat($('#framingRPM').val()),
         framingCoolant: $('#framingCoolant').val(),
+        framingSides: $('#framingSides').val()
     };
-    console.log(data);
 
     if (data.framingFinalDepth > data.framingDepth) {
         console.log("multipass")
@@ -164,9 +186,12 @@ function createFramingGcode() {
         data.framingFinalDepth = data.framingDepth
     }
 
+    console.log('store config');
+    console.log(data);
+    console.log(JSON.stringify(data));
     localStorage.setItem("lastFramingTool", JSON.stringify(data));
 
-    var startpointX = 0 - data.framingDiameter ;
+    var startpointX = 0 - data.framingDiameter;
     var endpointX = data.framingX + data.framingDiameter;
 
     var startpointY = 0 - data.framingDiameter;
@@ -197,15 +222,18 @@ M3 S` + data.framingRPM + `; Spindle On
     }
 
     gcode += `G4 P1.8; Wait for spindle to come up to speed
-G0 Z10; Move to Safe Height
+G1 Z10; Move to Safe Height
 G0 X0 Y0; Move to origin position
 G1 F` +
         data.framingFeedrate + `; Set feedrate\n\n`;
 
-    gcode += `G0 X` + startpointX.toFixed(4) + ` Y` + startpointY.toFixed(4) + `; move to framing start point\n`;
+    //gcode += `G0 X` + startpointX.toFixed(4) + ` Y` + startpointY.toFixed(4) + `; move to framing start point\n\n`;
 
     // MULTIPASS
+    var counter = 1;
     for (q = data.framingDepth; q < data.framingFinalDepth + data.framingDepth; q += data.framingDepth) {
+        gcode += `; start of round ` + counter + `\n`;
+
         if (q > data.framingFinalDepth) {
             var zval = -data.framingFinalDepth;
         } else {
@@ -213,15 +241,64 @@ G1 F` +
         }
         console.log(q, zval)
 
-        gcode += `G1 Z` + zval + `; plunge to depth for this round\n`;
-        gcode += `G1 X` + startpointX.toFixed(4) + ` Y` + endpointY.toFixed(4) + ` Z` + zval + `; cut left side\n`;
-        gcode += `G0 X` + endpointX.toFixed(4) + ` Y` + endpointY.toFixed(4) + ` Z` + zval + `; cut top side\n`;
-        gcode += `G1 X` + endpointX.toFixed(4) + ` Y` + startpointY.toFixed(4) + ` Z` + zval + `; cut right side\n`;
-        gcode += `G0 X` + startpointX.toFixed(4) + ` Y` + startpointY.toFixed(4) + ` Z` + zval + `; cut bottom side\n`;
+        if (data.framingSides === "all" || data.framingSides === "left" || data.framingSides === "leftRight") {
+            gcode += `G1 Z10; lifting to Z safe height\n`;
+            gcode += `G1 X` + startpointX.toFixed(4) + ` Y` + startpointY.toFixed(4) + `; move to left side\n`;
+
+            // cut along left side
+            gcode += `G1 Z` + zval + `; plunge to depth for this round\n`;
+            gcode += `G1 X` + startpointX.toFixed(4) + ` Y` + endpointY.toFixed(4) + `; cut left side\n`;
+
+            if (data.framingSides === "leftRight") {
+                gcode += `G1 Z10; lifting to Z safe height\n`;
+                gcode += `G1 X` + endpointX.toFixed(4) + ` Y` + endpointY.toFixed(4) + `; move to right side\n`;
+            }
+        }
+
+        if (data.framingSides === "all" || data.framingSides === "top" || data.framingSides === "topBottom") {
+            if (data.framingSides === "top" || data.framingSides === "topBottom") {
+                gcode += `G1 Z10; lifting to Z safe height\n`;
+                gcode += `G1 X` + startpointX.toFixed(4) + ` Y` + endpointY.toFixed(4) + `; move to top side\n`;
+            }
+
+            // cut along top side
+            gcode += `G1 Z` + zval + `; plunge to depth for this round\n`;
+            gcode += `G1 X` + endpointX.toFixed(4) + ` Y` + endpointY.toFixed(4) + `; cut top side\n`;
+
+            if (data.framingSides === "topBottom") {
+                gcode += `G1 Z10; lifting to Z safe height\n`;
+                gcode += `G1 X` + endpointX.toFixed(4) + ` Y` + startpointY.toFixed(4) + `; cut bottom side\n`;
+            }
+        }
+
+        if (data.framingSides === "all" || data.framingSides === "right" || data.framingSides === "leftRight") {
+            if (data.framingSides === "right" || data.framingSides === "leftRight") {
+                gcode += `G1 Z10; lifting to Z safe height\n`;
+                gcode += `G1 X` + endpointX.toFixed(4) + ` Y` + endpointY.toFixed(4) + `; move to top right side\n`;
+            }
+
+            // cut along right side
+            gcode += `G1 Z` + zval + `; plunge to depth for this round\n`;
+            gcode += `G1 X` + endpointX.toFixed(4) + ` Y` + startpointY.toFixed(4) + `; cut right side\n`;
+        }
+
+        if (data.framingSides === "all" || data.framingSides === "bottom" || data.framingSides === "topBottom") {
+            if (data.framingSides === "bottom" || data.framingSides === "topBottom") {
+                gcode += `G1 Z10; lifting to Z safe height\n`;
+                gcode += `G1 X` + endpointX.toFixed(4) + ` Y` + startpointY.toFixed(4) + `; move to bottom right side\n`;
+            }
+
+            // cut along bottom side
+            gcode += `G1 Z` + zval + `; plunge to depth for this round\n`;
+            gcode += `G1 X` + startpointX.toFixed(4) + ` Y` + startpointY.toFixed(4) + `; cut bottom side\n`;
+        }
+
+        counter++;
     }
     // END MULTIPASS
 
-    gcode += `G0 Z10; pass complete, lifting to Z safe height\n\n`;
+    gcode += `G1 Z10; pass complete, lifting to Z safe height\n\n`;
+    gcode += `G0 X` + startpointX.toFixed(4) + ` Y` + startpointY.toFixed(4) + `; move to framing start point\n`;
     gcode += `M5 S0; Spindle Off\n`;
 
     if (data.framingCoolant === "enabled") {
